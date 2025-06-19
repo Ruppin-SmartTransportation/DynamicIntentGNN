737e04d2a7ff6d57fbd972ea4bf32504642501db
Guy Tordjman <turgibot@gmail.com>
2025-06-19 15:54:50 +0300

    initial commit

---
+++ models/astgnn.py
@@ -0,0 +1,9 @@
+import torch.nn as nn
+
+class ASTGNN(nn.Module):
+    def __init__(self, **kwargs):
+        super().__init__()
+        # Define ASTGNN layers here
+
+    def forward(self, x):
+        pass
---
+++ models/stgnn.py
@@ -0,0 +1,9 @@
+import torch.nn as nn
+
+class STGNN(nn.Module):
+    def __init__(self, **kwargs):
+        super().__init__()
+        # Define STGNN layers here
+
+    def forward(self, x):
+        pass
